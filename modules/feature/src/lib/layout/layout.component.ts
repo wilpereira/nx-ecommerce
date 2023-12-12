8fd9e41e4d2560b7cadcf18c0bfbb8abb1fa9ab2
William Pereira <wil.scp@gmail.com>
2023-12-11 23:04:09 -0300

    feat(layout): add lib layout

---
+++ modules/feature/src/lib/layout/layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nx-ecommerce-layout',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+})
+export class LayoutComponent {}
